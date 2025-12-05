38f32bcb146fb3ec0cdda7d7eccc24d3d7d63a8f
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2025-12-05 22:06:51 +0900

    abc092_a

---
+++ ABC/092/092_a.py
@@ -0,0 +1,18 @@
+# 問題URL: https://atcoder.jp/contests/abc092/tasks/abc092_a
+# アルゴリズム: 
+
+def main():
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+
+    ans = min([a, b]) + min([c, d])
+
+    return ans
+
+def main_class():
+    pass
+
+if __name__ == "__main__":
+    print(main())
