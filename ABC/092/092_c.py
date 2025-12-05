d046cf4cca6e9d8b2b00ae7977598e4c70ef8b51
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2025-12-05 23:41:51 +0900

    092_c

--- ABC/092/092_c.py
+++ ABC/092/092_c.py
@@ -1,3 +1,18 @@
-# 問題URL: 
-# アルゴリズム: 
+# 問題URL: https://atcoder.jp/contests/abc092/tasks/arc093_a
 
+
+def main():
+    num = int(input())
+    spots = [0] + list(map(int, input().split())) + [0]
+    total_money = 0
+
+    for i in range(num + 1):
+        total_money = total_money + abs(spots[i] - spots[i+1])
+    for i in range(1, num + 1):
+        original = abs(spots[i-1] - spots[i]) + abs(spots[i] - spots[i+1])
+        shortcut = abs(spots[i-1] - spots[i+1])
+        money = total_money - original + shortcut
+        print(money)
+
+if __name__ == "__main__":
+    main()
