d3b5ca0074254fc3f4077ddf9a92b09b7127e954
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2025-12-09 14:06:01 +0900

    ABC112_b

--- ABC/112/112_b.py
+++ ABC/112/112_b.py
@@ -1,14 +1,11 @@
 """
-問題URL: 
+問題URL: https://atcoder.jp/contests/abc112/tasks/abc112_b
 ----------------------------------------------------
 結果
-・
-
-なぜ解けなかった？
-・
+・自力（10min）
 
 解法ポイント、学び
-・
+・空リストはFalseとみなされる
 ----------------------------------------------------
 """
 import sys
@@ -16,7 +13,18 @@ import sys
 input = sys.stdin.readline
 
 def main():
-
+    n, t = map(int, input().split())
+    c = []
+    for _ in range(n):
+        c_i, t_i = map(int, input().split())
+        if t_i <= t:
+            c.append(c_i)
+    
+    if c == []:
+    # if not c: の方が良い（[]はFalseとみなされる）
+        print("TLE")
+    else:
+        print(min(c))
 
 if __name__ == "__main__":
     main()
