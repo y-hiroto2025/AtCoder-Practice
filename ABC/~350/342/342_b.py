40c7d7eccb000c6df209d0e46454bf8c317d04cc
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-01-02 11:30:38 +0900

    ABC342_b

--- ABC/~350/342/342_b.py
+++ ABC/~350/342/342_b.py
@@ -2,10 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc342/tasks/abc342_b
 ----------------------------------------------------
 結果
-・
-
-なぜ解けなかった？
-・
+・自力（7min）
 
 解法ポイント、学び
 ・
@@ -16,6 +13,22 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+    P = list(map(int, input().split()))
+
+    posision = {person: i for i, person in enumerate(P)}
+
+    Q = int(input())
+    for i in range(Q):
+        A_i, B_i = map(int, input().split())
+        """front = min(P.index(A_i), P.index(B_i))
+        print(P[front])時間がかかる"""
+
+        if posision[A_i] < posision[B_i]:
+            print(A_i)
+        else:
+            print(B_i)
+
 
 
 if __name__ == "__main__":
