12a4fa318683785f617e7a10a4712e67863c0d65
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-01-20 11:25:55 +0900

    ABC156_C

--- ABC/~200/156/156_c.py
+++ ABC/~200/156/156_c.py
@@ -2,13 +2,10 @@
 問題URL: https://atcoder.jp/contests/abc156/tasks/abc156_c
 ----------------------------------------------------
 結果
-・
-
-なぜ解けなかった？
-・
+・自力（6min）
 
 解法ポイント、学び
-・
+・２乗和の合計が最小になるのは平均値。
 ----------------------------------------------------
 """
 import sys
@@ -16,7 +13,21 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+    X = list(map(int, input().split()))
+    """current_min = float('inf')
 
+    for p in range(min(X), max(X) + 1):
+        consumption = sum((x - p)**2 for x in X)
 
+        current_min = min(current_min, consumption)
+
+    print(current_min)"""
+
+    avg = sum(X) / N
+    P = int(avg + 0.5) # 四捨五入
+    ans = sum((x - P) ** 2 for x in X)
+    print(ans)
+    
 if __name__ == "__main__":
     main()
