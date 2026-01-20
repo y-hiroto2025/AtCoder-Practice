d72f5210269a95dd9e35fb252f50272ce2a6c798
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-01-20 11:36:06 +0900

    ABC121_B

--- ABC/~150/121/121_b.py
+++ ABC/~150/121/121_b.py
@@ -2,21 +2,29 @@
 問題URL: https://atcoder.jp/contests/abc121/tasks/abc121_b
 ----------------------------------------------------
 結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
-----------------------------------------------------
+・自力（6min）
+----------------------------------------------
 """
 import sys
 
 input = sys.stdin.readline
 
 def main():
+    N, M, C = map(int, input().split())
+    B = list(map(int, input().split()))
+    ans = 0
 
+    for _ in range(N):
+        A_i = list(map(int, input().split()))
+
+        """if sum(A_i[j] * B[j] for j in range(M)) + C > 0:
+            ans += 1"""
+        
+        total = sum(a * b for a, b in zip(A_i, B))
+        if total + C > 0:
+            ans += 1
+
+    print(ans)
 
 if __name__ == "__main__":
     main()
