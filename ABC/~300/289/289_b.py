674e5b9863820909ef8a498cde4790b062097c96
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-01-21 12:21:46 +0900

    ABC289_B

--- ABC/~300/289/289_b.py
+++ ABC/~300/289/289_b.py
@@ -1,14 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc289/tasks/abc289_b
 ----------------------------------------------------
-結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
 ----------------------------------------------------
 """
 import sys
@@ -16,7 +8,19 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N, M = map(int, input().split())
+    a = set(map(int, input().split()))
 
+    stack = []
+
+    for i in range(1, N + 1):
+        stack.append(i)
+
+        if i not in a:
+            print(*stack[::-1], end=" ")
+
+            stack = []
+    print()
 
 if __name__ == "__main__":
     main()
