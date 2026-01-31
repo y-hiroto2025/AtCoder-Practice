2b10a7f8db60431497bface4017e3c0fe8c2306f
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-01-31 16:22:05 +0900

    ABC343_C

--- ABC/~350/343/343_c.py
+++ ABC/~350/343/343_c.py
@@ -1,14 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc343/tasks/abc343_c
 ----------------------------------------------------
-結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
 ----------------------------------------------------
 """
 import sys
@@ -16,7 +8,17 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
 
+    ans_set = set()
+    for i in range(1000000):
+        K = i ** 3
+        if K > N:
+            break
+        else:
+            if str(K) == str(K)[::-1]:
+                ans_set.add(K)
+    print(max(ans_set))
 
 if __name__ == "__main__":
     main()
