91c7835e37de3726b812185a089cb0436d3502ce
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-01-31 16:11:10 +0900

    ABC262_B

--- ABC/~300/262/262_b.py
+++ ABC/~300/262/262_b.py
@@ -1,14 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc262/tasks/abc262_b
 ----------------------------------------------------
-結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
 ----------------------------------------------------
 """
 import sys
@@ -16,7 +8,24 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N, M = map(int, input().split())
 
+    conb = [[False] * N for _ in range(N)]
+
+    for _ in range(M):
+        U_i, V_i = map(int, input().split())
+        conb[U_i - 1][V_i - 1] = True
+        conb[V_i - 1][U_i - 1] = True
+    
+    ans = 0
+
+    for a in range(N):
+        for b in range(a + 1, N):
+            for c in range(b + 1, N):
+                if conb[a][b] and conb[b][c] and conb[c][a]:
+                    ans += 1
+    
+    print(ans)
 
 if __name__ == "__main__":
     main()
