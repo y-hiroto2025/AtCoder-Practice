54e43b903704ff3b83c59f24ba954fcbb44bed45
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-02-01 16:03:16 +0900

    ABC328_C

--- ABC/~350/328/328_c.py
+++ ABC/~350/328/328_c.py
@@ -1,14 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc328/tasks/abc328_c
 ----------------------------------------------------
-結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
 ----------------------------------------------------
 """
 import sys
@@ -16,6 +8,19 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N, Q = map(int, input().split())
+    S = input().strip()
+    prefix = [0] * N
+
+    for i in range(1, N):
+        if S[i] == S[i - 1]:
+            prefix[i] = prefix[i - 1] + 1
+        else:
+            prefix[i] = prefix[i - 1]
+    
+    for _ in range(Q):
+        l_i, r_i = map(int, input().split())
+        print(prefix[r_i - 1] - prefix[l_i - 1])
 
 
 if __name__ == "__main__":
