37a3502ba2e36ddd92c8bc6c33fdbde945e5d377
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-02-02 11:07:49 +0900

    ABC037

--- ABC/~050/037/037_b.py
+++ ABC/~050/037/037_b.py
@@ -2,13 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc037/tasks/abc037_b
 ----------------------------------------------------
 結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
+・自力（3min）
 ----------------------------------------------------
 """
 import sys
@@ -16,7 +10,18 @@ import sys
 input = sys.stdin.readline
 
 def main():
-
+    N, Q = map(int, input().split())
+    a = [0] * N
+    
+    for _ in range(Q):
+        L_i, R_i, T_i = map(int, input().split())
+        for i in range(L_i - 1, R_i):
+            a[i] = T_i
+        """
+        a[L_i - 1: R_i] = [T_i] * (R_i - L_i + 1)
+        """
+    
+    print(*a, sep="\n")
 
 if __name__ == "__main__":
     main()
