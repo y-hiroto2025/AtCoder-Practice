cae679ee7114f8cd981971176912d7cc53f74eaf
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-02-04 11:03:35 +0900

    ABC367_C

--- ABC/~400/367/367_c.py
+++ ABC/~400/367/367_c.py
@@ -2,21 +2,22 @@
 問題URL: https://atcoder.jp/contests/abc367/tasks/abc367_c
 ----------------------------------------------------
 結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
+・ギブアップ
 ----------------------------------------------------
 """
 import sys
+import itertools
 
 input = sys.stdin.readline
 
 def main():
+    N, K = map(int, input().split())
+    R = list(map(int, input().split()))
 
-
+    ranges = [range(1, R[i] + 1) for i in range(N)]
+    for comb in itertools.product(*ranges):
+        if sum(comb) % K == 0:
+            print(*comb)
+            
 if __name__ == "__main__":
     main()
