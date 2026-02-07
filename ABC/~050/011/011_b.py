54392889638fd8c1d6d96079dbbefc52b624e09b
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-02-07 11:36:26 +0900

    ABC011_B

--- ABC/~050/011/011_b.py
+++ ABC/~050/011/011_b.py
@@ -2,21 +2,16 @@
 問題URL: https://atcoder.jp/contests/abc011/tasks/abc011_b
 ----------------------------------------------------
 結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
+・自力（3min）
 ----------------------------------------------------
 """
-import sys
-
-input = sys.stdin.readline
-
 def main():
-
+    S = input().strip()
+    """if len(S) > 1:
+        print(S[0].upper() + S[1:].lower())
+    else:
+        print(S.upper())"""
+    print(S.capitalize())
 
 if __name__ == "__main__":
     main()
