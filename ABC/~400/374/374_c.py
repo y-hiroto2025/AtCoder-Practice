25f27ebb647352605e03334887c2d585842c8612
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-02-07 11:57:27 +0900

    ABC374_C

--- ABC/~400/374/374_c.py
+++ ABC/~400/374/374_c.py
@@ -1,21 +1,28 @@
 """
 問題URL: https://atcoder.jp/contests/abc374/tasks/abc374_c
 ----------------------------------------------------
-結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
 ----------------------------------------------------
 """
 import sys
+import itertools
 
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+    K = list(map(int, input().split()))
+
+    ans = float('inf')
+    total_K = sum(K)
+
+    for pattern in itertools.product([0, 1], repeat=N):
+        S_A = sum(list(K[i] * pattern[i] for i in range(N)))
+        S_B = total_K - S_A
+
+        current_score = max(S_A, S_B)
+
+        ans = min(ans, current_score)
+    print(ans)
 
 
 if __name__ == "__main__":
