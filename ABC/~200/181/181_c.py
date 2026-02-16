9210eba70f2eb9b1bd41af6cf56365553d0105c7
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-02-16 11:12:14 +0900

    ABC181_C

--- ABC/~200/181/181_c.py
+++ ABC/~200/181/181_c.py
@@ -1,14 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc181/tasks/abc181_c
 ----------------------------------------------------
-結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
 ----------------------------------------------------
 """
 import sys
@@ -16,6 +8,22 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+
+    X, Y = [], []
+    for _ in range(N):
+        x, y = map(int, input().split())
+        X.append(x)
+        Y.append(y)
+
+    for i in range(N - 2):
+        for j in range(i + 1, N - 1):
+            for k in range(j + 1, N):
+                if (Y[k]-Y[i])*(X[j]-X[i]) == (Y[j]-Y[i])*(X[k]-X[i]):
+                    print("Yes")
+                    return
+    
+    print("No")
 
 
 if __name__ == "__main__":
