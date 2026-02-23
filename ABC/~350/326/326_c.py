3a6bf8ed430c3cb2b3242e482e567842f19d6ebe
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-02-23 11:34:06 +0900

    ABC326_C

--- ABC/~350/326/326_c.py
+++ ABC/~350/326/326_c.py
@@ -1,21 +1,22 @@
 """
 問題URL: https://atcoder.jp/contests/abc326/tasks/abc326_c
 ----------------------------------------------------
-結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
 ----------------------------------------------------
 """
 import sys
-
+import bisect
 input = sys.stdin.readline
 
 def main():
+    N, M = map(int, input().split())
+    A = sorted(map(int, input().split()))
+
+    ans = 0
+    for i in range(N):
+        r = bisect.bisect_left(A, A[i]+M)
+        ans = max(ans, r-i)
+
+    print(ans)
 
 
 if __name__ == "__main__":
