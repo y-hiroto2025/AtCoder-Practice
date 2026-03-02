f5e90b56ad24c967337f43b555b91cbff0bf7993
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-03-02 11:20:16 +0900

    ABC370_B

--- ABC/~400/370/370_b.py
+++ ABC/~400/370/370_b.py
@@ -2,13 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc370/tasks/abc370_b
 ----------------------------------------------------
 結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
+・自力（13min）
 ----------------------------------------------------
 """
 import sys
@@ -16,7 +10,17 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+    A = [list(map(int, input().split())) for _ in range(N)]
 
+    ans = A[0][0]
+    for i in range(1, N):
+        if ans >= i+1:
+            ans = A[ans-1][i]
+        else:
+            ans = A[i][ans-1]
+
+    print(ans)
 
 if __name__ == "__main__":
     main()
