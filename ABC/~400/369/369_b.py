1c81a97177b6331c17814bc80c4d98136c3e84b9
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-03-02 11:04:02 +0900

    ABC369_B

--- ABC/~400/369/369_b.py
+++ ABC/~400/369/369_b.py
@@ -2,13 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc369/tasks/abc369_b
 ----------------------------------------------------
 結果
-・
-
-なぜ解けなかった？
-・
-
-解法ポイント、学び
-・
+・自力（13min）
 ----------------------------------------------------
 """
 import sys
@@ -16,6 +10,26 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+
+    last_r = 0
+    last_l = 0
+    ans = 0
+
+    for i in range(N):
+        a, s = input().split()
+        a = int(a)
+        if s == 'R':
+            if last_r != 0:
+                ans += abs(a - last_r)
+            last_r = a
+
+        else:
+            if last_l != 0:
+                ans += abs(a - last_l)
+            last_l = a
+    
+    print(ans)
 
 
 if __name__ == "__main__":
