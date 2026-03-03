de22adddad4297fcbaf69e83d15b2faba4008332
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-03-03 10:26:14 +0900

    ABC396_C

--- ABC/~400/396/396_c.py
+++ ABC/~400/396/396_c.py
@@ -9,11 +9,21 @@ input = sys.stdin.readline
 
 def main():
     N, M = map(int, input().split())
-    B = sorted(map(int, input().split()), reverse=True)
-    W = sorted(map(int, input().split()), reverse=True)
+    B = sorted(map(int, input().split()), reverse=True) #黒の価値
+    W = sorted(map(int, input().split()), reverse=True) #白の価値
 
+    ans = 0
+    current = 0
     for i in range(N):
-        
+
+        current += B[i]
+
+        if i < M and W[i] > 0:
+            current += W[i]
+
+        ans = max(ans, current)
+
+    print(ans)
 
 
 if __name__ == "__main__":
