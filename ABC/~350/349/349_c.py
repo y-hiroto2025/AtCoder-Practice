132576e969895e8a1d2d35307fe29c9527ed9af5
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-03-05 19:34:07 +0900

    ABC349_C

--- ABC/~350/349/349_c.py
+++ ABC/~350/349/349_c.py
@@ -1,15 +1,25 @@
 """
 問題URL: https://atcoder.jp/contests/abc349/tasks/abc349_c
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
-import sys
-
-input = sys.stdin.readline
-
 def main():
+    S = input().strip()
+    T = input().strip().lower()
+
+    idx = 0
+    for s in S:
+
+        if s == T[idx]:
+            idx += 1
+        
+        if idx == 3:
+            break
+    
+    if idx == 3 or (idx == 2 and T[2] == "x"):
+        print("Yes")
+    else:
+        print("No")
 
 
 if __name__ == "__main__":
