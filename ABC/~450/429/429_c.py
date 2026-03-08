d21031f2a21a5cc2ab12f10807f4446f1197180f
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-03-08 10:00:17 +0900

    ABC429_C

--- ABC/~450/429/429_c.py
+++ ABC/~450/429/429_c.py
@@ -1,15 +1,24 @@
 """
 問題URL: https://atcoder.jp/contests/abc429/tasks/abc429_c
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
 import sys
+from collections import Counter
 
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    ans = 0
+
+    num_count = Counter(A)
+    for count in num_count.values():
+        ans += (count*(count-1))//2 * (N-count)
+    
+    print(ans)
 
 
 if __name__ == "__main__":
