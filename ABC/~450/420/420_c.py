8dc7a6410f79cd13dd1e3a22b3c85bd643bfdeba
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-03-09 11:19:05 +0900

    ABC420_C

--- ABC/~450/420/420_c.py
+++ ABC/~450/420/420_c.py
@@ -1,8 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc420/tasks/abc420_c
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +8,30 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N, Q = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    min_sum = [min(A[i], B[i]) for i in range(N)]
+    ans = sum(min_sum)
+
+    for _ in range(Q):
+        c, X, V = input().split()
+        X = int(X) - 1
+        V = int(V)
+
+        old_min = min(A[X], B[X])
+
+        if c == "A":
+            A[X] = V
+        else:
+            B[X] = V
+        
+        new_min = min(A[X], B[X])
+
+        ans += new_min - old_min
+        
+        print(ans)
 
 
 if __name__ == "__main__":
