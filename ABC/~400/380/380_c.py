4fa9e5cae291c49fa8d10d07a6c15911e0b8a118
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-03-11 11:46:04 +0900

    ABC380_C

--- ABC/~400/380/380_c.py
+++ ABC/~400/380/380_c.py
@@ -1,15 +1,33 @@
 """
 問題URL: https://atcoder.jp/contests/abc380/tasks/abc380_c
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
-import sys
-
-input = sys.stdin.readline
-
 def main():
+    N, K = map(int, input().split())
+    S = input().strip()
+
+    S_list = []
+    s = [S[0]]
+    for i in range(1, N):
+        if s[-1] == S[i]:
+            s.append(S[i])
+        else:
+            S_list.append("".join(s))
+            s = [S[i]]
+        
+        if i == N-1:
+            S_list.append("".join(s))
+
+    count = 0
+    for idx, s in enumerate(S_list):
+        if s[0] == "1":
+            count += 1
+            
+        if count == K:
+            S_list[idx], S_list[idx-1] = S_list[idx-1], S_list[idx]
+            print("".join(S_list))
+            return
 
 
 if __name__ == "__main__":
