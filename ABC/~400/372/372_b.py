38b065254603cf2849e85a7bdef9ba2f35406400
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-03-26 10:36:37 +0900

    ABC372_B

--- ABC/~400/372/372_b.py
+++ ABC/~400/372/372_b.py
@@ -2,15 +2,18 @@
 問題URL: https://atcoder.jp/contests/abc372/tasks/abc372_b
 ----------------------------------------------------
 結果
-・
+・自力（10min）
 ----------------------------------------------------
 """
-import sys
-
-input = sys.stdin.readline
-
 def main():
+    M = int(input())
 
+    ans = []
+    for i in range(11):
+        ans += [i] * (M % 3)
+        M //= 3
+    print(len(ans))
+    print(*ans)
 
 if __name__ == "__main__":
     main()
