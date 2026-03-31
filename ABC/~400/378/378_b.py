28f4a73670a1b0d456a166fa7851c93c2cde7f06
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-01 07:42:49 +0900

    ABC377_B

--- ABC/~400/378/378_b.py
+++ ABC/~400/378/378_b.py
@@ -1,33 +1,15 @@
 """
-問題URL: https://atcoder.jp/contests/abc378/tasks/abc378_b
+問題URL: https://atcoder.jp/contests/abc377/tasks/abc377_b
 ----------------------------------------------------
 結果
-・自力（7min）
+・
 ----------------------------------------------------
 """
+import sys
+
+input = sys.stdin.readline
+
 def main():
-    S = list(input().strip() for _ in range(8))
-
-    risk_x = set()
-    risk_y = set()
-
-    for i in range(8):
-        for j in range(8):
-
-            if S[i][j] == "#":
-                risk_x.add(i)
-                risk_y.add(j)
-
-    ans = 0
-    for i in range(8):
-        if i in risk_x:
-            continue
-        
-        for j in range(8):
-            if j not in risk_y:
-                ans += 1
-
-    print(ans)
 
 
 if __name__ == "__main__":
