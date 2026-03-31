416839e44d859353152c32f798fa97ea8dadf861
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-01 08:13:19 +0900

    ABC378_B

--- ABC/~400/378/378_b.py
+++ ABC/~400/378/378_b.py
@@ -1,8 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc377/tasks/abc377_b
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +8,28 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+
+    Q, R = [], []
+    for _ in range(N):
+        q, r = map(int, input().split())
+        Q.append(q)
+        R.append(r)
+    
+    Ques = int(input())
+    for _ in range(Ques):
+        t, d = map(int, input().split())
+
+        t -= 1
+        b, c = d // Q[t], d % Q[t]
+        if c <= R[t]:
+            a = b
+        else:
+            a = b + 1
+        
+        ans = a * Q[t] + R[t]
+
+        print(ans)
 
 
 if __name__ == "__main__":
