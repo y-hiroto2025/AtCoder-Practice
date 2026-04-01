de2da203abb36a7754733d34bddf47d4d6ff2245
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-02 08:15:51 +0900

    ABC380_B

--- ABC/~400/380/380_b.py
+++ ABC/~400/380/380_b.py
@@ -1,35 +1,23 @@
 """
-問題URL: https://atcoder.jp/contests/abc380/tasks/abc380_b
+問題URL: https://atcoder.jp/contests/abc379/tasks/abc379_b
 ----------------------------------------------------
 結果
-・自力（10min）
+・自力（3min）
 ----------------------------------------------------
 """
 def main():
-    N, K = map(int, input().split())
-    S = input().strip()
+    S = input().lstrip("|")
 
-    cnt = 0
-    eat = True
-    ans = 0
-
-    for i in range(N):
-        if S[i] == "O":
-
-            if eat == False:
-                cnt = 1
-                eat = True
-            else:
-                cnt += 1
-
+    ans = []
+    curr = 0
+    for i in range(len(S)):
+        if S[i] == "-":
+            curr += 1
         else:
-            eat = False
-        
-        if cnt == K:
-            ans += 1
-            cnt = 0
-    
-    print(ans)
+            ans.append(curr)
+            curr = 0
+
+    print(*ans)
 
 
 if __name__ == "__main__":
