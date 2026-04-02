f9e699a720ef2657da73c201e34279575f88fbc9
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-03 07:59:07 +0900

    ABC382_B

--- ABC/~400/382/382_b.py
+++ ABC/~400/382/382_b.py
@@ -2,7 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc382/tasks/abc382_b
 ----------------------------------------------------
 結果
-・
+・自力（4min）
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +10,19 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N, D = map(int, input().split())
+    S = [s for s in input().strip()]
+
+    cnt = 0
+    for i in range(N):
+        if S[N-i - 1] == "@":
+            cnt += 1
+            S[N-i - 1] = "."
+        
+        if cnt == D:
+            break
+    
+    print(*S, sep="")
 
 
 if __name__ == "__main__":
