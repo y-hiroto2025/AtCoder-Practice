0b3390d2589e43269b9dcbd80877033337df573a
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-04 08:27:50 +0900

    ABC384_B

--- ABC/~400/385/385_b.py
+++ ABC/~400/385/385_b.py
@@ -2,7 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc384/tasks/abc384_b
 ----------------------------------------------------
 結果
-・
+・自力（15min）
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +10,34 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    H, W, X, Y = map(int, input().split())
+    X -= 1
+    Y -= 1
+
+    s = []
+    for _ in range(H):
+        s.append(input().strip())
+    
+    T = input().strip()
+
+    cood_move = {"U": (-1,0), "D": (1,0), "L": (0,-1), "R": (0,1)}
+    house_set = set()
+
+    for t in T:
+        x_move, y_move = cood_move[t][0], cood_move[t][1]
+
+        if s[X + x_move][Y + y_move] == ".":
+            X += x_move
+            Y += y_move
+        
+        elif s[X + x_move][Y + y_move] == "@":
+            X += x_move
+            Y += y_move
+            house_set.add((X, Y))
+    
+    print(X+1, Y+1, len(house_set))
+
+
 
 
 if __name__ == "__main__":
