5a02e3ca674c89014f3b687531a12c2e7a97a491
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-09 14:01:57 +0900

    ABC388_B

--- ABC/~400/388/388_b.py
+++ ABC/~400/388/388_b.py
@@ -2,7 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc388/tasks/abc388_b
 ----------------------------------------------------
 結果
-・
+・自力（3min）
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +10,20 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N, D = map(int, input().split())
+
+    snake = []
+    for _ in range(N):
+        t, l = map(int, input().split())
+        snake.append((t, l))
+    
+    for k in range(1, D + 1):
+        ans = 0
+        for i in range(N):
+            ans = max(ans, snake[i][0] * (snake[i][1] + k))
+        
+        print(ans)
+
 
 
 if __name__ == "__main__":
