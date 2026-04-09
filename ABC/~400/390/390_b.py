1d188c92b613a4a0e42cbd66f2148d8062bfaebd
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-09 14:09:15 +0900

    ABC390_B

--- ABC/~400/390/390_b.py
+++ ABC/~400/390/390_b.py
@@ -1,8 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc390/tasks/abc390_b
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +8,15 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    for i in range(N-2):
+        if A[i]*A[i+2] != A[i+1]*A[i+1]:
+            print("No")
+            return
+    
+    print("Yes")
 
 
 if __name__ == "__main__":
