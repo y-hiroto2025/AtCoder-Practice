be6c7373adacd8886337f50068bd013d8bdbaa0f
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-19 12:51:00 +0900

    ABC402_B

--- ABC/~450/402/402_b.py
+++ ABC/~450/402/402_b.py
@@ -2,7 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc402/tasks/abc402_b
 ----------------------------------------------------
 結果
-・
+・自力（3min）
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +10,17 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    Q = int(input())
+
+    orders = []
+
+    for _ in range(Q):
+        querry = list(map(int, input().split()))
+
+        if querry[0] == 1:
+            orders.append(querry[1])
+        else:
+            print(orders.pop(0))
 
 
 if __name__ == "__main__":
