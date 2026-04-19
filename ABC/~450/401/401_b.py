b6cc8ac74d957371a169e25f837b711018250119
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-19 12:46:54 +0900

    ABC401_B

--- ABC/~450/401/401_b.py
+++ ABC/~450/401/401_b.py
@@ -1,8 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc401/tasks/abc401_b
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +8,23 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+
+    log = False
+    ans = 0
+
+    for _ in range(N):
+        s = input().strip()
+
+        if s == "login":
+            log = True
+        if s == "logout":
+            log = False
+
+        if s == "private" and not log:
+            ans += 1
+    
+    print(ans)
 
 
 if __name__ == "__main__":
