d5ecbe2d371820f44b753ab4e11b00c30d69e429
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-22 10:13:33 +0900

    ABC406_B

--- ABC/~450/406/406_b.py
+++ ABC/~450/406/406_b.py
@@ -2,7 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc406/tasks/abc406_b
 ----------------------------------------------------
 結果
-・
+・自力（3min）
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +10,19 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    ans = A[0]
+
+    for i in range(1, N):
+
+        if len(str(ans * A[i])) > K:
+            ans = 1
+        else:
+            ans *= A[i]
+        
+    print(ans)
 
 
 if __name__ == "__main__":
