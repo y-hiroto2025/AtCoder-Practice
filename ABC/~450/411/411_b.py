2d6ed84280f24e30779e3b0f53f18016f6ef50e7
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-23 14:52:08 +0900

    ABC411_B

--- ABC/~450/411/411_b.py
+++ ABC/~450/411/411_b.py
@@ -1,8 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc411/tasks/abc411_b
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +8,16 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+    D = list(map(int, input().split()))
+
+    for i in range(N-1):
+        ans = [D[i]]
+
+        for j in range(i+1, N-1):
+            ans.append(ans[-1] + D[j])
+
+        print(*ans)   
 
 
 if __name__ == "__main__":
