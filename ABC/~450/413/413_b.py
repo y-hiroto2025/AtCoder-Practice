9cfa0b8c517dd74f2f187af6b045d5634c35e5d8
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-25 11:32:35 +0900

    ABC413_B

--- ABC/~450/413/413_b.py
+++ ABC/~450/413/413_b.py
@@ -2,14 +2,22 @@
 問題URL: https://atcoder.jp/contests/abc413/tasks/abc413_b
 ----------------------------------------------------
 結果
-・
+・自力（3min）
 ----------------------------------------------------
 """
-import sys
-
-input = sys.stdin.readline
-
 def main():
+    N = int(input())
+    S = [input().strip() for _ in range(N)]
+
+    ans_set = set()
+
+    for i in range(N):
+        for j in range(N):
+
+            if i != j:
+                ans_set.add(S[i] + S[j])
+
+    print(len(ans_set))
 
 
 if __name__ == "__main__":
