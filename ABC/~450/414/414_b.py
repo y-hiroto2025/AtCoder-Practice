7898ca32512289d08066321941b80daf090edbae
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-25 11:52:12 +0900

    ABC414_B

--- ABC/~450/414/414_b.py
+++ ABC/~450/414/414_b.py
@@ -2,7 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc414/tasks/abc414_b
 ----------------------------------------------------
 結果
-・
+・自力（7min）
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +10,28 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+
+    ans = ""
+    len_ans = 0
+
+    c_l = []
+    for _ in range(N):
+        c, l = input().split()
+        l = int(l)
+        len_ans += l
+
+        c_l.append((c, l))
+
+    if len_ans > 100:
+        print("Too Long")
+        return
+    
+    for i in range(N):
+        ans += c_l[i][0] * c_l[i][1]
+    
+    print(ans)
+
 
 
 if __name__ == "__main__":
