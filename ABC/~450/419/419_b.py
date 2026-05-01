dc821d8ccf75a6e5c7f3649e3db43620d677c739
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-05-01 20:59:51 +0900

    ABC419_B

--- ABC/~450/419/419_b.py
+++ ABC/~450/419/419_b.py
@@ -2,7 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc419/tasks/abc419_b
 ----------------------------------------------------
 結果
-・
+・自力（8min）
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +10,29 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    Q = int(input())
+
+    ball = {}
+
+    for _ in range(Q):
+        query = list(map(int, input().split()))
+        
+        if query[0] == 1:
+
+            if query[1] in ball:
+                ball[query[1]] += 1
+            else:
+                ball[query[1]] = 1
+        
+        else:
+            for key in sorted(ball.keys()):
+                
+                if ball[key] >= 1:
+                    print(key)
+                    ball[key] -= 1
+                    break
+        
+
 
 
 if __name__ == "__main__":
