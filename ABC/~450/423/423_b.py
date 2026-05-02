a2c6b7b1d2076a4e5ecf9e008e13a52d834a48f2
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-05-02 11:21:54 +0900

    ABC423_B

--- ABC/~450/423/423_b.py
+++ ABC/~450/423/423_b.py
@@ -1,8 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc423/tasks/abc423_b
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +8,23 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+    L = list(map(int, input().split()))
+
+    right_one = 0
+    left_one = 0
+
+    for i in range(N):
+        if L[i] == 1:
+            right_one = i
+            break
+    
+    for i in range(N):
+        if L[N-i-1] == 1:
+            left_one = N-i-1
+            break
+    
+    print(left_one - right_one)
 
 
 if __name__ == "__main__":
