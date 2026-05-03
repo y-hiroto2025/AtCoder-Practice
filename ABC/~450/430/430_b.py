3e683c9ac9410604bd2746ce875a4d4a4c0dc500
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-05-03 18:19:06 +0900

    ABC430_B

--- ABC/~450/430/430_b.py
+++ ABC/~450/430/430_b.py
@@ -1,8 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc430/tasks/abc430_b
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +8,23 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N, M = map(int, input().split())
+    S = [input().strip() for _ in range(N)]
+
+    pattern = set()
+
+    for i in range(N - M + 1):
+
+        for j in range(N - M + 1):
+            line = ""
+
+            for k in range(i, i+M):
+                line += S[k][j:j+M]
+        
+            pattern.add(line)
+    
+    print(len(pattern))
+
 
 
 if __name__ == "__main__":
