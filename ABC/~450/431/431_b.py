b24c74715f0ec6c298b1847bb138053c144a5703
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-05-03 18:27:03 +0900

    ABC431_B

--- ABC/~450/431/431_b.py
+++ ABC/~450/431/431_b.py
@@ -2,7 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc431/tasks/abc431_b
 ----------------------------------------------------
 結果
-・
+・自力（5min）
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +10,24 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    X = int(input())
+    N = int(input())
+    W = list(map(int, input().split()))
+
+    Q = int(input())
+    equips = []
+    
+    for _ in range(Q):
+        q = int(input()) - 1
+
+        if q not in equips:
+            equips.append(q)
+            X += W[q]
+        else:
+            equips.remove(q)
+            X -= W[q]
+        
+        print(X)
 
 
 if __name__ == "__main__":
