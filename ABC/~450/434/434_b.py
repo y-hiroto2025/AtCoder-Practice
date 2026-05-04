60d8665400abcd4d674640e0c0d708053c16db96
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-05-04 22:32:19 +0900

    ABC434_B

--- ABC/~450/434/434_b.py
+++ ABC/~450/434/434_b.py
@@ -2,7 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc434/tasks/abc434_b
 ----------------------------------------------------
 結果
-・
+・自力（4min）
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +10,18 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N, M = map(int, input().split())
+    bards = {}
+
+    for _ in range(N):
+        a, b = map(int, input().split())
+        if a in bards:
+            bards[a].append(b)
+        else:
+            bards[a] = [b]
+    
+    for k in range(M):
+        print(sum(bards[k+1]) / len(bards[k+1]))
 
 
 if __name__ == "__main__":
