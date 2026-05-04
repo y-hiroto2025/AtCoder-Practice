8fb4875091e90dfe26f1a08a7b90c7fbba239475
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-05-04 22:26:12 +0900

    ABC433_B

--- ABC/~450/433/433_b.py
+++ ABC/~450/433/433_b.py
@@ -2,7 +2,7 @@
 問題URL: https://atcoder.jp/contests/abc433/tasks/abc433_b
 ----------------------------------------------------
 結果
-・
+・自力（3min）
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +10,19 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    for i in range(N):
+        A_i = A[i]
+        ans = -1
+
+        for j in range(i+1):
+            if A_i < A[i-j]:
+                ans = i-j + 1
+                break
+        
+        print(ans)
 
 
 if __name__ == "__main__":
