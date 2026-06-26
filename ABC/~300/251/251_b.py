93b349e7f80e5e62d15baaa680b9ed607d465f8b
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-06-26 19:11:47 +0900

    ABC251_B

--- ABC/~300/251/251_b.py
+++ ABC/~300/251/251_b.py
@@ -1,8 +1,6 @@
 """
 問題URL: https://atcoder.jp/contests/abc251/tasks/abc251_b
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
 import sys
@@ -10,6 +8,32 @@ import sys
 input = sys.stdin.readline
 
 def main():
+    N, W = map(int, input().split())
+    A = list(map(int, input().split()))
+    num_set = set()
+
+    for i in range(N):
+        num = A[i]
+
+        if num <= W:
+            num_set.add(num)
+    
+    for i in range(N-1):
+        for j in range(i+1, N):
+            num = A[i]+A[j]
+
+            if num <= W:
+                num_set.add(num)
+
+    for i in range(N-2):
+        for j in range(i+1, N-1):
+            for k in range(j+1, N):
+                num = A[i]+A[j]+A[k]
+
+                if num <= W:
+                    num_set.add(num)
+    
+    print(len(num_set))
 
 
 if __name__ == "__main__":
