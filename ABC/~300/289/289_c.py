ccf5dae78584d3c83ea61c779e6b91bd6c3ab151
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-06-27 13:57:17 +0900

    ABC289_C

--- ABC/~300/289/289_c.py
+++ ABC/~300/289/289_c.py
@@ -1,15 +1,37 @@
 """
 問題URL: https://atcoder.jp/contests/abc289/tasks/abc289_c
 ----------------------------------------------------
-結果
-・
 ----------------------------------------------------
 """
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
 def main():
+    N, M = map(int, input().split())
+    A = []
+
+    for _ in range(M):
+        c = int(input())
+        a = set(map(int, input().split()))
+        A.append(a)
+    
+    ans = 0
+    
+    for k in range(1, M+1):
+        comb = list(combinations(A, k))
+
+        for c in comb:
+            n = set()
+
+            for i in range(len(c)):
+                n |= c[i]
+            
+            if len(n) == N:
+                ans += 1
+    
+    print(ans)
 
 
 if __name__ == "__main__":
